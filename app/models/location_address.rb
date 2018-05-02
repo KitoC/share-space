7b3258b3164d0787123198379bcf9f6747ac390c
Kito Clark <kitoclark1989@gmail.com>
2018-05-02 16:08:14 +1000

    Deleted redundant code from previous migrations

--- app/models/location_address.rb
+++
@@ -1,3 +0,0 @@
-class LocationAddress < ApplicationRecord
-  has_and_belongs_to_many :users
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   resources :collab_space_listings
   resources :addresses
-  resources :location_addresses
   devise_for :users
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
