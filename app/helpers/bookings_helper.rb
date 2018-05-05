867592e7373f04a4196068bf11b7eef7fa32bdb2
Kito Clark <kitoclark1989@gmail.com>
2018-05-05 14:58:43 +1000

    Added a condition to only disable dates on bookings if bookings have been paid for

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,13 +1,15 @@
 module BookingsHelper
 
+# This helper method loops through all the bookings belonging to the sharespace that was sent through with a id param. Checks each booking's date_from and date_to, separating them into individual day dates and pushes them into an array which is then returned to the view that is calling the method.
   def calendar_date_block(ss_id)
-    bookings = Sharespace.find(ss_id).bookings.all
+    bookings = Sharespace.find(ss_id).bookings.where(paid: true).all
     block_dates = []
     bookings.each do |booking|
       (booking.date_from..booking.date_to).each do |date|
-       block_dates.push(date)
+        block_dates.push(date)
       end
     end
     return block_dates
   end
+  
 end
