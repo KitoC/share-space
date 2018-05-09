227ea8f727b9a09805770ad53136454af0e767b2
Kito Clark <kitoclark1989@gmail.com>
2018-05-09 17:04:48 +1000

    Added more styling to SS show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myNavbar");
+    var x = document.getElementById("nav_two");
     if (x.className === "navbar") {
         x.className += " responsive";
     } else {
